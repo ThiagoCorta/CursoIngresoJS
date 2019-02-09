ba95d57cd3d7f5277f78ddae2df6d54b475ed1b3
Thiago Corta <thiago.corta@gmail.com>
2019-02-08 22:28:57 -0300

    Hasta el 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe1;
+	var descuento;
+	var resultado1;
+
+	importe=document.getElementById('importe').value;
+
+	importe1=parseInt(importe1);
+
+	descuento = importe1 * 25 / 100;
+
+	resultado1= importe1 - descuento;
+
+	document.getElementById('resultado').value=resultado1;
+
+
 }
