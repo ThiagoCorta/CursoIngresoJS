32ad881570482e5e4fc702126bb8a0894098cd96
Thiago Corta <thiago.corta@gmail.com>
2019-02-08 22:05:07 -0300

    Hasta el 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,22 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
+//aumento = sueldo * 10 / 100
+	var importe;
+	var aumento;
+	var resultado1;
 	
+	importe=document.getElementById('sueldo').value;
+
+	importe=parseInt(importe);
+	//aumento=parseInt(aumento);
+
+	aumento = importe * 10 / 100;
+
+	resultado1 = importe + aumento;
+
+	document.getElementById('resultado').value=resultado1;
+
+
+
 }
