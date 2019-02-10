7c95353b65536faa67efea8ed4c7752f1cb91cc6
Thiago Corta <47344748+ThiagoCorta@users.noreply.github.com>
2019-02-10 03:11:45 -0300

    Ejercicios 1 a 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
 
+	var precio;
+	var porcentaje;
+	var descuento;
+	var preciofinal;
+
+	precio=prompt("Ingrese el precio de su producto");
+	porcentaje=prompt("Ingrese el descuento de su producto");
+
+	descuento=precio*porcentaje/100;
+
+	preciofinal=precio - descuento;
+
+	document.getElementById('elPrecioFinal').value=preciofinal;
+
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,22 @@
 function mostrar()
 {
 
+
+	var base;
+	var altura;
+	var suma;
+	var resultado;
+
+	base=prompt("Ingrese la base del rectangulo en CM");
+	altura=prompt("Ingrese la altura del rectangulo en CM");
+
+	base=parseInt(base);
+	altura=parseInt(altura);
+
+	suma=base+altura;
+	resultado= 2 * suma; 
+
+
+	alert("El perimetro del rectango es: " + resultado + " cm2")
+
 }
