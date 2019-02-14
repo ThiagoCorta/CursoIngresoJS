74fe8ed0ccfc0e0e915ceac9cdfd79ba9cfff7a5
thiago corta <thiago.corta@gmail.com>
2019-02-14 20:25:59 -0300

    uno al cuatro

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+	var importe;
+	var propina;
+	var importeP;
+	var iva;
+	var importeI;
+	var importefinal;
+	var grupo;
+	var importegrupo;
+
+	importe=prompt("Ingresar el importe sin iva");
+	grupo=prompt("Ingresar entre cuantas personas pagan")
+	
+	importe=parseInt(importe);
+	grupo=parseInt(grupo);
+
+
+	propina=importe * 10 / 100;
+	importeP=importe+propina;
+	iva=importe * 21 / 100;
+	importeI=importe+iva;
+	importefinal=importeP+iva;
+	importegrupo=importefinal / grupo;
+
+	alert("El importe + propina del 10% es: " + importeP + " + iva incluido es: " + importeI + " importe final = " + importefinal + " Entre " + grupo + " amigos el importe de cada uno es " + importegrupo)
+
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
-  
+  var compra;
+  var descuento;
+  var compra1;
+  var iva
+  var comprafinal;
+
+  compra=prompt("Ingrese el valor de su compra");
+
+  compra=parseInt(compra);
+
+
+  descuento=compra * 10 / 100;
+  compra1= compra - descuento;
+  iva= compra1 *21 /100;
+  comprafinal = compra1 + iva;
+
+  alert("Tu compra es de " + compra + " tenes un  descuento del 10% queda en " + compra1 + ", mas el iva es " + comprafinal)
 }
