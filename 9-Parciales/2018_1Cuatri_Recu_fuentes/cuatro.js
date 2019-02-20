d9cea210f56281eacc86f9786ea9576f92689c5d
thiago <thiago.corta@gmail.com>
2019-02-20 14:31:51 -0300

    cuatro

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,33 +1,95 @@
 function mostrar()
 {
-	var importe;
-	var propina;
-	var importeP;
-	var iva;
-	var importeI;
-	var importefinal;
-	var grupo;
-	var importegrupo;
+	// mostrar el num mayor
+	//desc 10% si es mayor q 100 se muestra eso de la suma total.
+	// mayor q 50 5% de desc
+	// debajo de 50 tiene q pagar un 15% mas
 
-	importe=prompt("Ingresar el importe sin iva");
-	grupo=prompt("Ingresar entre cuantas personas pagan")
+	var precio1;
+	var precio2;
+	var precio3;
+	var precio4;
+	var suma;
+	var descuento;
+	var preciofinal;
+	var preciofinal1;
+	var mensaje;
+	var porcen;
+
+	precio1=prompt("Decime el precio 1");
+	precio2=prompt("Decime el precio 2");
+	precio3=prompt("Decime el precio 3");
+	precio4=prompt("Decime el precio 4");
+
+	precio1=parseInt(precio1);
+	precio2=parseInt(precio2);
+	precio3=parseInt(precio3);
+	precio4=parseInt(precio4);
+
+	suma=precio1 + precio2 + precio3 + precio4;
+	porcen=1;
 	
-	importe=parseInt(importe);
-	grupo=parseInt(grupo);
 
+	if(precio1> precio2 && precio3 && precio4)
+	{
+		mensaje=precio1
+	}
+	else
+	{
+		if (precio2> precio3 && precio4)
+		{
+			mensaje=precio2
+		}
 
-	propina=importe * 10 / 100;
-	importeP=importe+propina;
-	iva=importe * 21 / 100;
-	importeI=importe+iva;
-	importefinal=importeP+iva;
-	importegrupo=importefinal / grupo;
+			else
+			{
+				if (precio3> precio4)
+			{
+				mensaje=precio3
+			}
+				else 
+				{
+					mensaje=precio4
+				}
+			}
 
-	alert("El importe + propina del 10% es: " + importeP + " + iva incluido es: " + importeI + " importe final = " + importefinal + " Entre " + grupo + " amigos el importe de cada uno es " + importegrupo)
+			
+	}
 
+	
+	
 
+	if (suma>99)
+	{
+		porcen=10;
+		
+	}
 
+	else
+	{
+		if (suma>49)
+		{
+			porcen=5;
+			
+			
+		}
+		
+			else
+			{
+				porcen= -15;
+				
+			}
+	
+	}	
 
+	descuento= suma * porcen / 100;
+	preciofinal= suma-descuento;
+	
 
+	
+	
+	
+
+	alert("El numero mayor es " + mensaje + " El numero final es:" +preciofinal);
 
 }
