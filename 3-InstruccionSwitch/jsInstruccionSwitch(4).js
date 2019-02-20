a0832b960aba019ca369bcf1472f54520a222d18
Thiago Corta <47344748+ThiagoCorta@users.noreply.github.com>
2019-02-20 01:51:20 -0300

    Switch completo.

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,11 +2,48 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+var mensaje;
+
 
 //alert (mesDelAño);
+
+switch (mesDelAño)
+{
+
+		case "Febrero":
+			mensaje="Este me tiene 28 dias";
+			break;
+
+		case "Enero":
+		
+		case "Mayo":
+		
+		case "Julio":
+
+		case "Agosto":
+
+		case "Octubre":
+
+		case "Diciembre":
+
+		case "Marzo":
+			mensaje="Este mes tiene 31 dias."
+			break;
+
+		case "Abril":
+			
+		case "Junio":	
+		
+		case "Septiembre":
+
+		case "Noviembre":
+
+			mensaje="Este mes tiene 30 dias."
+			break;
+}
+
+	alert(mensaje)
 	
 	
-
-
 
 }//FIN DE LA FUNCIÓN
