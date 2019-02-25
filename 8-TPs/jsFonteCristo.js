fe5206b49c7fc82d24a45cfff62c0b40109d80c4
Thiago Corta <47344748+ThiagoCorta@users.noreply.github.com>
2019-02-24 22:38:45 -0300

    tps

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,96 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
+
+	var numeroIngresado;
+ 	var numPares;
+ 	var numImpares;
+ 	var numDivisibles;
+ 	var mensaje;
+ 	var contador;
+ 	var total;
+ 	var divisor;
+
+
+
 function ComenzarIngreso () 
 {
- 	
+
+ 	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+ 	contador=0;
+
 }
+
+function NumerosPares ()
+{
+
+ 	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+ 	contador=0;
+ 	total =0;
+ 	while(contador < numeroIngresado)
+	{
+		contador=contador+1;
+		
+		if(contador%2==0)
+		{	
+			console.log("Par: " + contador);
+			total=total+1;
+		}
+ 	}
+
+ 	alert("Total de num pares: " + total);
+ 	
+
+
+}
+
+function NumerosImpares ()
+{
+
+
+ 	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+
+ 	contador=0;
+ 	total =0;
+ 	while(contador < numeroIngresado)
+	{
+		contador=contador+1;
+		
+		if(contador%2!=0)
+		{
+			console.log("impar" + contador)
+			total=total+1;
+		}
+ 	}
+ 	
+ 	alert("Total de num impares: " + total);
+
+}
+
+function NumerosDivisibles ()
+{
+
+
+ 	numeroIngresado=document.getElementById('numero').value;
+ 	numeroIngresado=parseInt(numeroIngresado);
+
+ 	contador=0;
+ 	total =0;
+ 	divisor= "";
+ 	while(contador < 100)
+	{
+		contador=contador+1;
+		
+		if(contador % numeroIngresado ==0)
+		{
+			console.log("Divisible: " + contador + "|| cont " + contador + " % num " + numeroIngresado + " = " + (contador % numeroIngresado));
+			total=total+1;
+			divisor =contador + " ";
+		}
+ 	}
+ 	alert("Del 1-100. Divisibles por " + numeroIngresado + " = " + total + " | Son:" + divisor);
+}
+
--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -10,4 +10,65 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
 function ComenzarIngreso () 
 {
  
+ 	var edad1;
+ 	var sexo1;
+ 	var estadoC;
+ 	var sueldoBruto;
+ 	var legajo1;
+ 	var nacionalidad1;
+
+
+
+
+ 			edad1=prompt("Ingresar edad de 18 a 90");
+ 			edad1=parseInt(edad1);
+ 		while(edad1 <= 17 || edad1 >= 91)
+		{
+			edad1=prompt("Error, ingresa edad valida de 18 a 90");
+			edad1=parseInt(edad1);
+			
+		}
+
+			sexo1=prompt("Ingresar sexo F O M");
+	
+		while(sexo1 != "f" && sexo1 != "m")
+		{
+			sexo1=prompt("Error ingresar sexo F O M");
+			
+		}
+		
+			estadoC=prompt("Ingresar estado civil 1=S 2=C 3=d 4=V");
+		while(estadoC<1 || estadoC>4)
+		{
+			estadoC=prompt("Error ingresar estado civil valido");
+			
+		}
+
+			sueldoBruto=prompt("Ingresar sueldo bruto no menor a 8000");
+			sueldoBruto=parseInt(sueldoBruto);
+		while(sueldoBruto<8000)
+		{
+			sueldoBruto=prompt("Error, ingresar sueldo bruto no menor a 8000");
+			sueldoBruto=parseInt(sueldoBruto);
+		}
+
+			legajo1=prompt("Ingresar legajo numero 4 cifras sin ceros a la isq");
+		while(legajo1<1000 || legajo1>9999)
+		{
+			legajo1=prompt("Error ,ingresar legajo numero 4 cifras sin ceros a la isq");
+		}
+
+			nacionalidad1=prompt("Ingresar Nacionalidad: A= Arg, E= extranjeros, N= nacionalizados");
+		while(nacionalidad1!="a" && nacionalidad1!="e" && nacionalidad1!="n")
+		{
+			nacionalidad1=prompt("Error,ingresar Nacionalidad: A= Arg, E= extranjeros, N= nacionalizados");
+		}
+
+
+		document.getElementById('Edad').value=edad1;
+		document.getElementById('Sexo').value=sexo1;
+		document.getElementById('EstadoCivil').value=estadoC;
+		document.getElementById('Sueldo').value=sueldoBruto;
+		document.getElementById('Legajo').value=legajo1;
+		document.getElementById('Nacionalidad').value=nacionalidad1;
 }
